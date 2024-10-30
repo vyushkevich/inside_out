296b6b98e72da76375200f63072da849fd505a93
Yuliana-Alcala <alcalabyulianac@gmail.com>
2024-10-30 17:46:47 +0100

    create Class Moment

---
+++ src/main/java/dev/inside_out/Moment.java
@@ -0,0 +1,53 @@
+package dev.inside_out;
+import java.time.LocalDate;
+
+public class Moment {
+    private String titleOfMoment;
+    private LocalDate dateOfMoment;
+    private String description;
+    private Emotion emotion;
+
+    public Moment(String title, LocalDate date, String description, Emotion emotion) {
+        this.titleOfMoment = title;
+        this.dateOfMoment = date;
+        this.description = description;
+        this.emotion = emotion;
+    }
+
+    public String getTitleOfMoment() {
+        return titleOfMoment;
+    }
+
+    public void setTitleOfMoment(String titleOfMoment) {
+        this.titleOfMoment = titleOfMoment;
+    }
+
+    public LocalDate getDateOfMoment() {
+        return dateOfMoment;
+    }
+
+    public void setDateOfMoment(LocalDate dateOfMoment) {
+        this.dateOfMoment = dateOfMoment;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Emotion getEmotion() {
+        return emotion;
+    }
+
+    public void setEmotion(Emotion emotion) {
+        this.emotion = emotion;
+    }
+   
+    
+
+    
+    
+}
