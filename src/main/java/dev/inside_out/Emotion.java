261d5e3d55e7ec90a0de5c199261cd4aefad9bde
NepyAnna <nepyann@gmail.com>
2024-11-05 21:24:18 +0100

    move function createEmotion to class Emotion, refactored the code

--- src/main/java/dev/inside_out/Emotion.java
+++ src/main/java/dev/inside_out/Emotion.java
@@ -1,5 +1,8 @@
 package dev.inside_out;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Emotion {
     private Integer id;
     private String nameOfEmotion;
@@ -15,5 +18,20 @@ public class Emotion {
 
     public String getNameOfEmotion() {
         return nameOfEmotion;
+    }
+
+    public static List<Emotion> createEmotions() {
+        List<Emotion> emotions = new ArrayList<>();
+        emotions.add(new Emotion(1, "Alegría"));
+        emotions.add(new Emotion(2, "Tristeza"));
+        emotions.add(new Emotion(3, "Ira"));
+        emotions.add(new Emotion(4, "Asco"));
+        emotions.add(new Emotion(5, "Miedo"));
+        emotions.add(new Emotion(6, "Ansiedad"));
+        emotions.add(new Emotion(7, "Envidia"));
+        emotions.add(new Emotion(8, "Vergüenza"));
+        emotions.add(new Emotion(9, "Aburrimiento"));
+        emotions.add(new Emotion(10, "Nostalgia"));
+        return emotions;
     }
 }
