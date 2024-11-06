b19b9dbc5d3b22dd5f9a9a3bcc1e25c9059e8b27
NepyAnna <nepyann@gmail.com>
2024-11-06 23:51:39 +0100

    edit code

--- src/main/java/dev/inside_out/App.java
+++ src/main/java/dev/inside_out/App.java
@@ -1,19 +1,11 @@
 package dev.inside_out;
 
-/**
- * Hello world!
- */
 public final class App {
     private App() {
     }
 
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
     public static void main(String[] args) {
         ProgramController controller = new ProgramController();        
-        controller.startProgram();
-       
+        controller.startProgram(); 
     }  
 }
--- src/test/java/dev/inside_out/EmotionTest.java
+++ src/test/java/dev/inside_out/EmotionTest.java
@@ -2,10 +2,7 @@ package dev.inside_out;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import java.util.List;
-
 import org.junit.jupiter.api.Test;
 
 public class EmotionTest {
